916b5e8c2ed97df550248a48dedae1536a560874
MatiasBertolotti <107021740+mbertolottismith@users.noreply.github.com>
2023-05-08 23:54:50 -0300

    Entrega 1
    Falta, modificar fotos de productos, agregar info de productos, agregar flechas para deslizar en la tienda.

---
+++ SITIO/slider.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    arrows: true,
+    prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-angle-left"></i></button>',
+    nextArrow: '<button type="button" class="slick-next"><i class="fas fa-angle-right"></i></button>',
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
